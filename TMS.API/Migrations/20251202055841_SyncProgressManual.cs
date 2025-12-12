443c706b7e8451c1f11a56cb292c2ad2d624989b
testing860 <testingaccount887@protonmail.com>
2025-12-12 08:56:56 +0100

    Migrations

---
+++ TMS.API/Migrations/20251202055841_SyncProgressManual.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TMS.API.Migrations
+{
+    public partial class SyncProgressManual : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally left blank
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally left blank
+        }
+    }
+}
